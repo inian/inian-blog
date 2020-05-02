60a95b78f870c10c71a3cbf36d98cd194a23084a
Inian Parameshwaran <inian1234@gmail.com>
2020-05-02 20:28:40 +0800

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,13 +60,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Everconfusedguy blog`,
+        short_name: `Everconfusedguy blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/question-mark.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
